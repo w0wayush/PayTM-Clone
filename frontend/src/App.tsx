e1591c5c1b8c344dd18e31cce9a33edbdbeb4794
w0wayush <ayupandey82@gmail.com>
2024-07-09 23:05:30 +0530

    Payment frontend completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+import Landing from "./pages/Landing";
 
 type Props = {};
 
 const App = (props: Props) => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/send" element={<SendMoney />}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
